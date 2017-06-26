c9a4cc999bac4ada6694d9950350232f77509049
Bianca Reis <bia.rbras@hotmail.com>
2017-06-26 18:24:14 -0300

    New ESLint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,11 @@ module.exports = {
 		'indent': ['warn', 'tab'],
 		'no-tabs': 'off',
 		'no-trailing-spaces': 'off',
+		'padded-blocks': ['error', {
+			'classes': 'always',
+			'blocks': 'never',
+			'switches': 'never'
+		}],
 		'space-before-function-paren': 'off',
 		'brace-style': ['warn', 'stroustrup', { 'allowSingleLine': true }]
 	}
