3e07e9e11f7de04f027f221500d4f28669fa3f15
Bianca Reis <bia.rbras@hotmail.com>
2017-11-15 22:05:05 -0200

    Creating tools

--- source/scripts/experiments/paint/PaintExperiment.js
+++ source/scripts/experiments/paint/PaintExperiment.js
@@ -1,5 +1,7 @@
+import Pug from 'pug'
 import Experiment from '../../classes/Experiment'
 import Pencil from './Pencil'
+import Rect from './Rect'
 
 export default class PaintExperiment extends Experiment {
 	
@@ -13,9 +15,13 @@ export default class PaintExperiment extends Experiment {
 		this.mainCanvas = null
 		this.mainContext = null
 		
+		this.tools = null
+		
 		this.configureCanvas()
 		this.createMainCanvas()
 		this.createMainContext()
+		
+		this.createTools()
 	}
 	
 	configureCanvas() {
@@ -60,11 +66,50 @@ export default class PaintExperiment extends Experiment {
 		this.context.clearRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight)
 	}
 	
+	createTools() {
+		const pug = `
+.tools_section
+	button(type="button").tools_button.js-clear Clear
+
+.tools_section
+	h2.tools_title Brushes
+
+	button.tools_button.js-pencil.-active-tool Pencil
+
+.tools_section
+	h2.tools_title Shapes
+
+	button.tools_line.js-line Line
+	button.tools_circle.js-circle Circle
+	button.tools_rect.js-rect Rect
+	button.tools_triangle.js-triangle Triangle
+
+.tools_section
+	h2.tools_title Color
+
+	input.tools_color.js-color(type="color")
+
+.tools_section
+	h2.tools_title Size
+
+	input.tools_size.js-size(type="range" min="1" max="100" value="10")
+	span.tools_size-value.js-size-value 10
+		`
+		
+		this.tools = document.createElement('div')
+		this.tools.classList.add('tools')
+		this.tools.innerHTML = Pug.render(pug)
+		
+		document.body.appendChild(this.tools)
+	}
+	
 	destroy() {
 		super.destroy()
 		
 		this.mainContext = null
 		this.mainCanvas.parentNode.removeChild(this.mainCanvas)
+		
+		this.tools.parentNode.removeChild(this.tools)
 	}
 	
 }
