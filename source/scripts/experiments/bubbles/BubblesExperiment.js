ab0f0c4777faeab768bdc366f50943c790e268b9
Bianca Reis <hello@biancarb.com>
2017-12-10 19:13:53 -0200

    Refactoring

--- source/scripts/experiments/bubbles/BubblesExperiment.js
+++ source/scripts/experiments/bubbles/BubblesExperiment.js
@@ -46,8 +46,8 @@ export default class BubblesExperiment extends Experiment {
 	mousemove(event) {
 		super.mousemove(event)
 		
-		this.x = event.clientX || event.touches[0].clientX
-		this.y = event.clientY || event.touches[0].clientY
+		this.x = event.type.startsWith('mouse') ? event.clientX : event.touches[0].clientX
+		this.y = event.type.startsWith('mouse') ? event.clientY : event.touches[0].clientY
 	}
 	
 	dblclick() {
